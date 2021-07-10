7a8c6020f2b76d530ffabf4014b983f300a5793e
Ali Raza <alisabri022@gmail.com>
2021-07-10 20:12:29 +0500

    hello

--- index.js
+++ index.js
@@ -48,10 +48,11 @@ app.use((error, req, res, next) => {
     },
   });
 });
+
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("/client/build"));
+  app.use(express.static("client/build"));
   app.get("*", (req, res) => {
-    res.sendFile(pathh.resolve(__dirname, "/client", "/build", "index.html"));
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
 }
 

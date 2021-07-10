857d9918c845d6db86d50fe48c1d8eddc05db486
Ali Raza <alisabri022@gmail.com>
2021-07-10 21:16:27 +0500

    hello

--- index.js
+++ index.js
@@ -49,7 +49,7 @@ app.use((error, req, res, next) => {
   });
 });
 // Step 1:
-app.use(express.static(path.resolve(__dirname, "client/build")));
+app.use("/", express.static(path.resolve(__dirname, "client/build")));
 // Step 2:
 app.get("/*", function (request, response) {
   response.sendFile(path.resolve(__dirname, "client/build", "index.html"));

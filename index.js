05b5814d52aefde59d1742b3859ae8d0a560c325
Ali Raza <alisabri022@gmail.com>
2021-07-10 20:29:51 +0500

    new addition

--- index.js
+++ index.js
@@ -50,9 +50,11 @@ app.use((error, req, res, next) => {
 });
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  // Step 1:
+  app.use(express.static(path.resolve(__dirname, "./client/build")));
+  // Step 2:
+  app.get("*", function (request, response) {
+    response.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
   });
 }
 

01aeae6e3e7c8ef1d30dec2e705fe766c9e5c0eb
Ali Raza <alisabri022@gmail.com>
2021-07-10 19:27:23 +0500

    removed the error

--- client/src/Components/Footer/index.js
+++ client/src/Components/Footer/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 import Typography from "@material-ui/core/Typography";
-import { makeStyles } from "@material-ui/core/Styles";
+import { makeStyles } from "@material-ui/core/styles";
 
 const useStyles = makeStyles((theme) => ({
   footer: {
